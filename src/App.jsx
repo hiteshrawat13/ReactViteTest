bb401c8e016a8eb2875949f3f751aface5337d11
hiteshrawat13 <hiteshrawat13@gmail.com>
2024-07-24 22:04:52 +0530

    a

--- src/App.jsx
+++ src/App.jsx
@@ -14,9 +14,9 @@ function App() {
   return (
     <>
       
-        {/* <Dashboard/>   */}
+    <Dashboard/> 
 
-        <MyComponent/>   
+      {/* <MyComponent/>    */}
       
     </>
   )
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -17,7 +17,8 @@ import Test from "../Test/Test"
 const Router=[
     {
     
-      element: /*Auth()?*/<App/> /*: <Login/>*/,
+      element: Auth()?<App/> : <Login/>,
+      
       children: [
         {
           path: "/",
